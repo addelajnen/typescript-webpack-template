94164517df2b84df477d4858dc58839a63956b30
addelajnen <addelajnen@outlook.com>
2020-02-03 21:28:16 +0100

    Updated dependencies

--- src/classes/PiProvider.ts
+++ src/classes/PiProvider.ts
@@ -2,7 +2,7 @@ export default class PiProvider {
     private readonly PI = 3.14;
 
     public async fetchPI() {
-        return new Promise((resolve, reject) => {
+        return new Promise(resolve => {
             const DELAY = 2000;
             setTimeout(() => {
                 return resolve(this.PI);
